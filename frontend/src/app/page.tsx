eac2cb3b76abb8f71e49b9adb340ac20b4f3c7ca
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-12-10 22:37:31 +0900

    ✨ feat: shadcn導入

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
