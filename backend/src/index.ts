c22ca003b3e8d2a4cc253141896f20b88a0b9777
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-12-14 22:07:06 +0900

    ✨ feat: 必要そうなmiddlewareの導入

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,20 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import { prettyJSON } from 'hono/pretty-json';
 
 const app = new Hono()
+app.use(prettyJSON());
+app.use(logger());
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}
+));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-})
+});
 
-export default app
+export default app;
