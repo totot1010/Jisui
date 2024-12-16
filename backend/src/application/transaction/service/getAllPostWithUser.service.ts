4935a4eac876561d9a2498d13eee569dd90bf7e2
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-12-17 01:20:53 +0900

    ✨ feat: GetAllPostWithUserServiceを追加し、投稿とユーザー情報を統合する機能を実装

---
+++ backend/src/application/transaction/service/getAllPostWithUser.service.ts
@@ -0,0 +1,35 @@
+import { PostQueryService } from "../../post/service/postQuery.service";
+import { UserQueryService } from "../../user/service/UserQuery.service";
+import { getAllPostWithUserDto } from "../dto/getAllPostWithUser.dto";
+
+export class GetAllPostWithUserService {
+  constructor(
+    private readonly postQueryService: PostQueryService,
+    private readonly userQueryService: UserQueryService
+  ) { }
+
+  public async execute(): Promise<getAllPostWithUserDto[]> {
+    const posts = await this.postQueryService.findAll();
+    const users = await this.userQueryService.findAll();
+
+    const result: getAllPostWithUserDto[] = [];
+
+    for (const post of posts) {
+      const user = users.find((user) => user.getUserId().value === post.getUserId().value);
+      if (!user) {
+        throw new Error("ユーザーが見つかりませんでした");
+      }
+      result.push(new getAllPostWithUserDto(
+        post.getPostId().value,
+        post.getTitle().value,
+        post.getPrice().value,
+        post.getUserId().value,
+        user.getUsername().value,
+        post.getCreateAt(),
+        post.getUpdatedAt()
+      ));
+    }
+
+    return result;
+  }
+}
