e0e720a008b0455d424a75db1dac5cc9b80500e6
RyotaAbe1014 <bm1000bmmm@gmail.com>
2025-01-08 21:50:17 +0900

    ✨feat: ローディングUI作成

--- frontend/src/feature/users/components/ProfileCard/ProfileCard.tsx
+++ frontend/src/feature/users/components/ProfileCard/ProfileCard.tsx
@@ -1,9 +1,13 @@
-import { ChevronLeft } from "lucide-react"
-import { UserInfo } from "../UserInfo"
 import { Suspense } from "react"
-import { CookingCount } from "../CookingCount"
-import { CookingHeatMap } from "../CookingHeatMap"
 import Link from "next/link"
+import { ChevronLeft } from "lucide-react"
+
+import { CookingCount } from "../CookingCount"
+import { CookingCountSkeleton } from "../CookingCount/CookingCountSkeleton"
+import { CookingHeatMap } from "../CookingHeatMap"
+import { CookingHeatMapSkeleton } from "../CookingHeatMap/CookingHeatMapSkeleton"
+import { UserInfo } from "../UserInfo"
+import { UserInfoSkeleton } from "../UserInfo/UserInfoSkeleton"
 
 type ProfileCardProps = {
   userId: string
@@ -17,26 +21,26 @@ export const ProfileCard = async ({ userId }: ProfileCardProps) => {
         <span>みんなの投稿</span>
       </Link>
       <div className="bg-white rounded-xl shadow-lg p-6 mb-8">
-        <Suspense fallback={<p>loading...</p>}>
+        <Suspense fallback={<UserInfoSkeleton />}>
           <UserInfo userId={userId} />
         </Suspense>
       </div>
       <div className="grid grid-cols-2 gap-4 mb-6">
         <div className="bg-gray-100 rounded-lg p-4">
           <h2 className="text-lg font-semibold mb-2">本日の自炊回数</h2>
-          <Suspense>
+          <Suspense fallback={<CookingCountSkeleton />}>
             <CookingCount targetPeriod="today" userId={userId} />
           </Suspense>
         </div>
         <div className="bg-gray-100 rounded-lg p-4">
           <h2 className="text-lg font-semibold mb-2">今週の自炊回数</h2>
-          <Suspense>
+          <Suspense fallback={<CookingCountSkeleton />}>
             <CookingCount targetPeriod="week" userId={userId} />
           </Suspense>
         </div>
         <div className="mb-6">
           <h2 className="text-lg font-semibold mb-2">年間自炊活動</h2>
-          <Suspense>
+          <Suspense fallback={<CookingHeatMapSkeleton />}>
             <CookingHeatMap userId={userId} />
           </Suspense>
         </div>
