fbdc0f9748a81ce5759fc644b1142f20aee75f16
RyotaAbe1014 <bm1000bmmm@gmail.com>
2025-01-08 21:57:37 +0900

    🩹 fix: レイアウト修正

--- frontend/src/feature/users/components/ProfileCard/ProfileCard.tsx
+++ frontend/src/feature/users/components/ProfileCard/ProfileCard.tsx
@@ -3,11 +3,11 @@ import Link from "next/link"
 import { ChevronLeft } from "lucide-react"
 
 import { CookingCount } from "../CookingCount"
-import { CookingCountSkeleton } from "../CookingCount/CookingCountSkeleton"
 import { CookingHeatMap } from "../CookingHeatMap"
-import { CookingHeatMapSkeleton } from "../CookingHeatMap/CookingHeatMapSkeleton"
 import { UserInfo } from "../UserInfo"
 import { UserInfoSkeleton } from "../UserInfo/UserInfoSkeleton"
+import { CookingCountSkeleton } from "../CookingCount/CookingCountSkeleton"
+import { CookingHeatMapSkeleton } from "../CookingHeatMap/CookingHeatMapSkeleton"
 
 type ProfileCardProps = {
   userId: string
@@ -24,19 +24,19 @@ export const ProfileCard = async ({ userId }: ProfileCardProps) => {
         <Suspense fallback={<UserInfoSkeleton />}>
           <UserInfo userId={userId} />
         </Suspense>
-      </div>
-      <div className="grid grid-cols-2 gap-4 mb-6">
-        <div className="bg-gray-100 rounded-lg p-4">
-          <h2 className="text-lg font-semibold mb-2">本日の自炊回数</h2>
-          <Suspense fallback={<CookingCountSkeleton />}>
-            <CookingCount targetPeriod="today" userId={userId} />
-          </Suspense>
-        </div>
-        <div className="bg-gray-100 rounded-lg p-4">
-          <h2 className="text-lg font-semibold mb-2">今週の自炊回数</h2>
-          <Suspense fallback={<CookingCountSkeleton />}>
-            <CookingCount targetPeriod="week" userId={userId} />
-          </Suspense>
+        <div className="grid grid-cols-2 gap-4 mb-6">
+          <div className="bg-gray-100 rounded-lg p-4">
+            <h2 className="text-lg font-semibold mb-2">本日の自炊回数</h2>
+            <Suspense fallback={<CookingCountSkeleton />}>
+              <CookingCount targetPeriod="today" userId={userId} />
+            </Suspense>
+          </div>
+          <div className="bg-gray-100 rounded-lg p-4">
+            <h2 className="text-lg font-semibold mb-2">今週の自炊回数</h2>
+            <Suspense fallback={<CookingCountSkeleton />}>
+              <CookingCount targetPeriod="week" userId={userId} />
+            </Suspense>
+          </div>
         </div>
         <div className="mb-6">
           <h2 className="text-lg font-semibold mb-2">年間自炊活動</h2>
