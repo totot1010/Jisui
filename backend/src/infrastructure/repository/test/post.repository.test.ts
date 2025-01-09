909c6639f1bee9a8965461a4f8f7bb2e1e5936d6
RyotaAbe1014 <bm1000bmmm@gmail.com>
2025-01-10 02:13:41 +0900

    ✅ test: pass test

--- backend/src/infrastructure/repository/test/post.repository.test.ts
+++ backend/src/infrastructure/repository/test/post.repository.test.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 
-import { afterEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { PostId, Price, Title } from "../../../domain/post/value_object";
 import { UserId } from "../../../domain/user/value_object";
 import { Post } from "../../../domain/post/entity/post.entity";
@@ -43,164 +43,204 @@ describe('PostRepository', async () => {
     await prisma.$disconnect()
   })
 
-  it('全ての投稿を取得できること', transactionTest(async () => {
-    // given
-    await userRepository.create(user);
+  describe('findAll', () => {
+    it('全ての投稿を取得できること', transactionTest(async () => {
+      // given
+      await userRepository.create(user);
 
-    const postId = PostId.generate();
-    const title = new Title('title');
-    const price = new Price(1000);
-    const userId = user.getUserId();
-    const createAt = new Date();
-    const updatedAt = new Date();
-    const post = new Post(postId, title, price, userId, createAt, updatedAt);
+      const postId = PostId.generate();
+      const title = new Title('title');
+      const price = new Price(1000);
+      const userId = user.getUserId();
+      const createAt = new Date();
+      const updatedAt = new Date();
+      const post = new Post(postId, title, price, userId, createAt, updatedAt);
 
-    const postId2 = PostId.generate();
-    const title2 = new Title('title2');
-    const price2 = new Price(2000);
-    const userId2 = user.getUserId();
-    const createAt2 = new Date();
-    const updatedAt2 = new Date();
-    const post2 = new Post(postId2, title2, price2, userId2, createAt2, updatedAt2);
+      const postId2 = PostId.generate();
+      const title2 = new Title('title2');
+      const price2 = new Price(2000);
+      const userId2 = user.getUserId();
+      const createAt2 = new Date();
+      const updatedAt2 = new Date();
+      const post2 = new Post(postId2, title2, price2, userId2, createAt2, updatedAt2);
 
-    await postRepository.create(post);
-    await postRepository.create(post2);
+      await postRepository.create(post);
+      await postRepository.create(post2);
 
-    // when
-    const results = await postRepository.findAll();
-    const [result2, result1] = results;
+      // when
+      const results = await postRepository.findAll();
+      const [result2, result1] = results;
 
-    // then
-    expect(results).toHaveLength(2);
-    expect(result1.getPostId().value).toBe(postId.value);
-    expect(result1.getTitle().value).toBe(title.value);
-    expect(result1.getPrice().value).toBe(price.value);
-    expect(result1.getUserId().value).toBe(userId.value);
-    expect(result2.getPostId().value).toBe(postId2.value);
-    expect(result2.getTitle().value).toBe(title2.value);
-    expect(result2.getPrice().value).toBe(price2.value);
-    expect(result2.getUserId().value).toBe(userId2.value);
-  }));
+      // then
+      expect(results).toHaveLength(2);
+      expect(result1.getPostId().value).toBe(postId.value);
+      expect(result1.getTitle().value).toBe(title.value);
+      expect(result1.getPrice().value).toBe(price.value);
+      expect(result1.getUserId().value).toBe(userId.value);
+      expect(result2.getPostId().value).toBe(postId2.value);
+      expect(result2.getTitle().value).toBe(title2.value);
+      expect(result2.getPrice().value).toBe(price2.value);
+      expect(result2.getUserId().value).toBe(userId2.value);
+    }));
+  });
 
-  it('ある投稿への同一ユーザーのいいねがある場合、Trueが返ってくること', transactionTest(async () => {
-    // given
-    await userRepository.create(user);
+  describe('countByUserIdAndType', () => {
+    beforeEach(() => {
+      // 時間をモック化
+      vi.useFakeTimers();
+    });
 
-    const postId = PostId.generate();
-    const title = new Title('title');
-    const price = new Price(1000);
-    const userId = user.getUserId();
-    const createAt = new Date();
-    const updatedAt = new Date();
-    const post = new Post(postId, title, price, userId, createAt, updatedAt);
+    afterEach(() => {
+      vi.useRealTimers();
+    });
 
-    await postRepository.create(post);
-    const like = new Like(userId, postId);
-    await postRepository.createLike(like);
+    it('ユーザーIDとタイプに基づいて投稿のカウントを取得できること', transactionTest(async () => {
+      // given
+      await userRepository.create(user);
+      const post1 = new Post(PostId.generate(), new Title('title1'), new Price(100), user.getUserId(), new Date(), new Date());
+      const post2 = new Post(PostId.generate(), new Title('title2'), new Price(200), user.getUserId(), new Date(), new Date());
 
-    // when
-    const result = await postRepository.existsLikeByUserAndPost(like);
+      await postRepository.create(post1);
+      await postRepository.create(post2);
 
-    // then
-    expect(result).toBeTruthy();
-  }));
+      // モック化した日付を設定
+      vi.setSystemTime(new Date('2023-01-01'));
 
-  it('ユーザーが投稿にいいねしていない場合、Falseが返ってくること', transactionTest(async () => {
-    // given
-    await userRepository.create(user);
+      // when
+      const countDay = await postRepository.countByUserIdAndType(user.getUserId(), 'day');
+      const countWeek = await postRepository.countByUserIdAndType(user.getUserId(), 'week');
 
-    const postId = PostId.generate();
-    const title = new Title('title');
-    const price = new Price(1000);
-    const userId = user.getUserId();
-    const createAt = new Date();
-    const updatedAt = new Date();
-    const post = new Post(postId, title, price, userId, createAt, updatedAt);
+      // then
+      expect(countDay).toBe(2);
+      expect(countWeek).toBe(2);
+    }));
+  });
 
-    await postRepository.create(post);
-    const like = new Like(userId, postId);
+  describe('existsLikeByUserAndPost', () => {
+    it('ある投稿への同一ユーザーのいいねがある場合、Trueが返ってくること', transactionTest(async () => {
+      // given
+      await userRepository.create(user);
 
-    // when
-    const result = await postRepository.existsLikeByUserAndPost(like);
+      const postId = PostId.generate();
+      const title = new Title('title');
+      const price = new Price(1000);
+      const userId = user.getUserId();
+      const createAt = new Date();
+      const updatedAt = new Date();
+      const post = new Post(postId, title, price, userId, createAt, updatedAt);
 
-    // then
-    expect(result).toBeFalsy();
-  }));
+      await postRepository.create(post);
+      const like = new Like(userId, postId);
+      await postRepository.createLike(like);
 
-  it('投稿にいいねをすることができること', transactionTest(async () => {
-    // given
-    await userRepository.create(user);
-    await userRepository.create(user2);
+      // when
+      const result = await postRepository.existsLikeByUserAndPost(like);
 
-    const postId = PostId.generate();
-    const title = new Title('title');
-    const price = new Price(1000);
-    const userId = user.getUserId();
-    const createAt = new Date();
-    const updatedAt = new Date();
-    const post = new Post(postId, title, price, userId, createAt, updatedAt);
+      // then
+      expect(result).toBeTruthy();
+    }));
 
-    const postId2 = PostId.generate();
-    const title2 = new Title('title2');
-    const price2 = new Price(2000);
-    const userId2 = user2.getUserId();
-    const createAt2 = new Date();
-    const updatedAt2 = new Date();
-    const post2 = new Post(postId2, title2, price2, userId2, createAt2, updatedAt2);
+    it('ユーザーが投稿にいいねしていない場合、Falseが返ってくること', transactionTest(async () => {
+      // given
+      await userRepository.create(user);
 
-    await postRepository.create(post);
-    await postRepository.create(post2);
+      const postId = PostId.generate();
+      const title = new Title('title');
+      const price = new Price(1000);
+      const userId = user.getUserId();
+      const createAt = new Date();
+      const updatedAt = new Date();
+      const post = new Post(postId, title, price, userId, createAt, updatedAt);
 
-    // when
-    const like1 = new Like(userId, postId);
-    await postRepository.createLike(like1);
-    const like2 = new Like(userId, postId2);
-    await postRepository.createLike(like2);
+      await postRepository.create(post);
+      const like = new Like(userId, postId);
 
-    // then
-    const result = await postRepository.existsLikeByUserAndPost(like1);
-    expect(result).toBeTruthy();
-    const result2 = await postRepository.existsLikeByUserAndPost(like2);
-    expect(result2).toBeTruthy();
-  }));
+      // when
+      const result = await postRepository.existsLikeByUserAndPost(like);
 
-  it('投稿へのいいねを外すことができること', transactionTest(async () => {
-    // given
-    await userRepository.create(user);
-    await userRepository.create(user2);
+      // then
+      expect(result).toBeFalsy();
+    }));
+  });
 
-    const postId = PostId.generate();
-    const title = new Title('title');
-    const price = new Price(1000);
-    const userId = user.getUserId();
-    const createAt = new Date();
-    const updatedAt = new Date();
-    const post = new Post(postId, title, price, userId, createAt, updatedAt);
+  describe('createLike', () => {
+    it('投稿にいいねをすることができること', transactionTest(async () => {
+      // given
+      await userRepository.create(user);
+      await userRepository.create(user2);
 
-    const postId2 = PostId.generate();
-    const title2 = new Title('title2');
-    const price2 = new Price(2000);
-    const userId2 = user2.getUserId();
-    const createAt2 = new Date();
-    const updatedAt2 = new Date();
-    const post2 = new Post(postId2, title2, price2, userId2, createAt2, updatedAt2);
+      const postId = PostId.generate();
+      const title = new Title('title');
+      const price = new Price(1000);
+      const userId = user.getUserId();
+      const createAt = new Date();
+      const updatedAt = new Date();
+      const post = new Post(postId, title, price, userId, createAt, updatedAt);
 
-    await postRepository.create(post);
-    await postRepository.create(post2);
+      const postId2 = PostId.generate();
+      const title2 = new Title('title2');
+      const price2 = new Price(2000);
+      const userId2 = user2.getUserId();
+      const createAt2 = new Date();
+      const updatedAt2 = new Date();
+      const post2 = new Post(postId2, title2, price2, userId2, createAt2, updatedAt2);
 
-    const like1 = new Like(userId, postId);
-    await postRepository.createLike(like1);
-    const like2 = new Like(userId, postId2);
-    await postRepository.createLike(like2);
+      await postRepository.create(post);
+      await postRepository.create(post2);
 
-    // when
-    await postRepository.deleteLike(like1);
-    await postRepository.deleteLike(like2);
+      // when
+      const like1 = new Like(userId, postId);
+      await postRepository.createLike(like1);
+      const like2 = new Like(userId, postId2);
+      await postRepository.createLike(like2);
 
-    // then
-    const result = await postRepository.existsLikeByUserAndPost(like1);
-    expect(result).toBeFalsy();
-    const result2 = await postRepository.existsLikeByUserAndPost(like2);
-    expect(result2).toBeFalsy();
-  }));
+      // then
+      const result = await postRepository.existsLikeByUserAndPost(like1);
+      expect(result).toBeTruthy();
+      const result2 = await postRepository.existsLikeByUserAndPost(like2);
+      expect(result2).toBeTruthy();
+    }));
+
+    describe('deleteLike', () => {
+      it('投稿へのいいねを外すことができること', transactionTest(async () => {
+        // given
+        await userRepository.create(user);
+        await userRepository.create(user2);
+
+        const postId = PostId.generate();
+        const title = new Title('title');
+        const price = new Price(1000);
+        const userId = user.getUserId();
+        const createAt = new Date();
+        const updatedAt = new Date();
+        const post = new Post(postId, title, price, userId, createAt, updatedAt);
+
+        const postId2 = PostId.generate();
+        const title2 = new Title('title2');
+        const price2 = new Price(2000);
+        const userId2 = user2.getUserId();
+        const createAt2 = new Date();
+        const updatedAt2 = new Date();
+        const post2 = new Post(postId2, title2, price2, userId2, createAt2, updatedAt2);
+
+        await postRepository.create(post);
+        await postRepository.create(post2);
+
+        const like1 = new Like(userId, postId);
+        await postRepository.createLike(like1);
+        const like2 = new Like(userId, postId2);
+        await postRepository.createLike(like2);
+
+        // when
+        await postRepository.deleteLike(like1);
+        await postRepository.deleteLike(like2);
+
+        // then
+        const result = await postRepository.existsLikeByUserAndPost(like1);
+        expect(result).toBeFalsy();
+        const result2 = await postRepository.existsLikeByUserAndPost(like2);
+        expect(result2).toBeFalsy();
+      }));
+    });
+  });
 });
