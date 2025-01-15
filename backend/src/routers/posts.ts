f78f02f8a8006a2ffc5d16b21f305bb106b379b1
yutocreate <1otuy26@gmail.com>
2025-01-16 00:47:07 +0900

    fix: エラーハンドリングを上書きしていた問題修正

--- backend/src/routers/posts.ts
+++ backend/src/routers/posts.ts
@@ -50,9 +50,11 @@ post.post("/likes", async (c) => {
   const postLikeService = new PostLikeService(postRepository)
   await postLikeService.toggleLike(likePostRequestDto);
   return c.json({ message: "" }, HttpStatus.OK);
-}).onError((error: any, c) => {
-  return c.json({ message: "いいねに失敗しました。" }, HttpStatus.INTERNAL_SERVER_ERROR);
-});
+})
+// .onError((error: any, c) => {
+//   console.log('error11', error);
+//   return c.json({ message: "いいねに失敗しました。" }, HttpStatus.INTERNAL_SERVER_ERROR);
+// });
 
 post.post("/comments", async (c) => {
   // ユーザーのコメントを作成する
