2106aad553f7dcb90bc7af9acf46799ce9eafbbc
RyotaAbe1014 <bm1000bmmm@gmail.com>
2025-01-27 01:34:41 +0900

    ✨ feat: Enhance middleware authentication and logout handling

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -10,15 +10,19 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next()
   }
 
+  console.log("middleware");
+
   const accessToken = request.cookies.get('accessToken')?.value
   const refreshToken = request.cookies.get('refreshToken')?.value
 
   if (!accessToken && !refreshToken) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    return handleLogout(request)
   }
+  console.log("accessToken is not valid");
 
   if (accessToken) {
     try {
+      console.log("accessToken is valid");
       const exp = decodeJwtTokenExp(accessToken)
       if (exp && exp > Date.now() / 1000) {
         return NextResponse.next()
@@ -26,18 +30,21 @@ export async function middleware(request: NextRequest) {
     }
     catch (error) {
       console.error('アクセストークンの検証に失敗しました:', error)
+      return handleLogout(request)
     }
 
     // アクセストークンが無効な場合、リフレッシュトークンを使用して新しいアクセストークンを取得
     if (refreshToken) {
       try {
+        console.log("refreshToken is valid");
         const response = NextResponse.next()
         const newAccessTokenResponse = await refreshAccessToken(refreshToken)
         if (isApiError(newAccessTokenResponse)) {
           console.error('リフレッシュトークンの更新に失敗しました:', newAccessTokenResponse)
-          return NextResponse.redirect(new URL('/login', request.url))
+          return handleLogout(request)
         }
 
+        console.log("newAccessTokenResponse", newAccessTokenResponse);
         response.cookies.set('accessToken', newAccessTokenResponse.data.accessToken, {
           httpOnly: true,
           secure: true,
@@ -46,17 +53,39 @@ export async function middleware(request: NextRequest) {
         return response
       } catch (error) {
         console.error('リフレッシュトークンの更新に失敗しました:', error)
+        return handleLogout(request)
       }
     }
 
     // 全てのトークンが無効な場合、ログインページにリダイレクト
-    const response = NextResponse.redirect(new URL('/login', request.url))
-    response.cookies.delete('accessToken')
-    response.cookies.delete('refreshToken')
-    return response
+    return handleLogout(request)
   }
 }
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
+
+const handleLogout = (request: NextRequest) => {
+  const url = new URL('/login', request.url)
+  const response = NextResponse.redirect(url, 303)
+  response.cookies.delete('accessToken')
+  response.cookies.delete('refreshToken')
+  response.cookies.delete('userId')
+  response.cookies.delete('username')
+  if (request.headers.get('Accept') === 'text/x-component') {
+    // Server Actionsでmiddleware.tsでリダイレクトを行うと、
+    // リダイレクトは実行されず画面遷移が行われないため、
+    // ミドルウェアでサーバーアクション応答を真似してレスポンスを返すことで、
+    // 画面遷移を行うようにしている
+    // この回避策は、Server Actionsではなく、ミドルウェアでリダイレクトを行う場合には不要
+    // https://github.com/vercel/next.js/issues/65394
+    return new NextResponse(null, {
+      status: 303,
+      headers: {
+        'X-Action-Redirect': url.toString()
+      }
+    });
+  }
+  return response
 }
