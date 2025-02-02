85dfb15ffb85adb783f1ea0b69382de3c3225188
RyotaAbe1014 <bm1000bmmm@gmail.com>
2025-02-02 21:11:04 +0900

    🧹 chore: Remove commented code in PostImageFakeRepository

--- backend/src/infrastructure/repository/postImage.fakeRepository.ts
+++ backend/src/infrastructure/repository/postImage.fakeRepository.ts
@@ -2,11 +2,6 @@ import { IPostImageRepository } from "../../domain/post/repository/postImage.rep
 import { PostId } from "../../domain/post/value_object";
 
 export class PostImageFakeRepository implements IPostImageRepository {
-  /*
-  ローカルでファイルを保存する
-  ファイルの保存先はupload/postImage/
-  */
-
   async upload(file: File): Promise<string> {
     return 'https://example.com/image.jpg';
   }
